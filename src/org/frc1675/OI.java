2a853d4fda4e6ab03d57354a6d4d2383fa5de02c
Josh Pordon <josh.pordon@gmail.com>
2013-01-26 07:19:37 -0600

    changed comment ;)

--- src/org/frc1675/OI.java
+++ src/org/frc1675/OI.java
@@ -53,7 +53,7 @@ public class OI {
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
 
-    //HOLY COMMENT holy shit
+    //HOLY COMMENT
     public OI() {
 
 
