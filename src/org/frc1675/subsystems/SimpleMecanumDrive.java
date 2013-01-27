08f0b5a7dec1e67ea8e0c7d4d53ffe240e4941f0
Eric Miller <agent.17k@gmail.com>
2013-01-27 14:57:32 -0600

    Renamed the SpeedControllers in SimpleMecanumDrive to be less vague. Also changed some other variable names slightly to be less vague.
    Swapped wheelSpeed variables, they were incorrectly placed. This is almost definitely the issue with the Mecanum code. Also changed the flips, in order to mirror the 2011 robot.

--- src/org/frc1675/subsystems/SimpleMecanumDrive.java
+++ src/org/frc1675/subsystems/SimpleMecanumDrive.java
@@ -19,16 +19,16 @@ public class SimpleMecanumDrive extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-    private SpeedController motor1;
-    private SpeedController motor2;
-    private SpeedController motor3;
-    private SpeedController motor4;
+    private SpeedController motorBL;
+    private SpeedController motorFL;
+    private SpeedController motorFR;
+    private SpeedController motorBR;
     
     public SimpleMecanumDrive(){
-        motor1 = new Victor(RobotMap.BACK_LEFT_DRIVE_MOTOR);
-        motor2 = new Victor(RobotMap.FRONT_LEFT_DRIVE_MOTOR);
-        motor3 = new Victor(RobotMap.FRONT_RIGHT_DRIVE_MOTOR);
-        motor4 = new Victor(RobotMap.BACK_RIGHT_DRIVE_MOTOR);
+        motorBL = new Victor(RobotMap.BACK_LEFT_DRIVE_MOTOR);
+        motorFL = new Victor(RobotMap.FRONT_LEFT_DRIVE_MOTOR);
+        motorFR = new Victor(RobotMap.FRONT_RIGHT_DRIVE_MOTOR);
+        motorBR = new Victor(RobotMap.BACK_RIGHT_DRIVE_MOTOR);
         
         
     }
@@ -40,20 +40,20 @@ public class SimpleMecanumDrive extends Subsystem {
         double cosD = Math.cos(dirInRad);
         
         double frWheelSpeed = cosD * magnitude - rotation;
-        double rrWheelSpeed = sinD * magnitude - rotation;
+        double brWheelSpeed = sinD * magnitude - rotation;
         double flWheelSpeed = cosD * magnitude + rotation;
-        double rlWheelSpeed = sinD * magnitude + rotation;
+        double blWheelSpeed = sinD * magnitude + rotation;
         
-        motor1.set(frWheelSpeed * -1);
-        motor2.set(rrWheelSpeed * -1);
-        motor3.set(flWheelSpeed);
-        motor4.set(rlWheelSpeed);
+        motorBL.set(blWheelSpeed);
+        motorFL.set(flWheelSpeed);
+        motorFR.set(frWheelSpeed * -1.0);
+        motorBR.set(brWheelSpeed * -1.0); //flips may change to other side
         /*f = front
-        * r = rear
+        * b = back
         * l = left
         * r = right */
         
-        System.out.println("Mecanum.drive: motor1:"+motor1.get()+", motor2: "+motor2.get()+", motor3: "+motor3.get()+", motor4: "+motor4.get());
+        System.out.println("Mecanum.drive: motor1:"+motorBL.get()+", motor2: "+motorFL.get()+", motor3: "+motorFR.get()+", motor4: "+motorBR.get());
         
     }
 
