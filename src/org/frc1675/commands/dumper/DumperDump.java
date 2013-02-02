7372ba5427c1bf8c9221dadbc67fab74f1d8874c
Josh Pordon <josh.pordon@gmail.com>
2013-02-01 21:19:38 -0600

    missed handling interruption of dump command

--- src/org/frc1675/commands/dumper/DumperDump.java
+++ src/org/frc1675/commands/dumper/DumperDump.java
@@ -48,5 +48,6 @@ public class DumperDump extends CommandBase {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
