ad19dca080942326742f6412a1b082d60a1ad7e6
Josh Pordon <josh.pordon@gmail.com>
2013-02-02 14:20:20 -0600

    Added Mecanum acceleration

--- src/org/frc1675/subsystems/drive/mecanum/SimpleMecanumDrive.java
+++ src/org/frc1675/subsystems/drive/mecanum/SimpleMecanumDrive.java
@@ -6,6 +6,7 @@ package org.frc1675.subsystems.drive.mecanum;
 
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.frc1675.RobotMap;
@@ -21,15 +22,26 @@ public class SimpleMecanumDrive extends Subsystem {
     private SpeedController motorFL;
     private SpeedController motorFR;
     private SpeedController motorBR;
+    private Timer rampTimer;
     
     public SimpleMecanumDrive(){
         motorBL = new Victor(RobotMap.BACK_LEFT_DRIVE_MOTOR);
         motorFL = new Victor(RobotMap.FRONT_LEFT_DRIVE_MOTOR);
         motorFR = new Victor(RobotMap.FRONT_RIGHT_DRIVE_MOTOR);
         motorBR = new Victor(RobotMap.BACK_RIGHT_DRIVE_MOTOR);
+        rampTimer = new Timer();
+        rampTimer.start();
     }
     
     public void drive(double magnitude, double direction, double rotation){
+        if(magnitude == 0.0){
+            rampTimer.reset(); //get ready to ramp
+        } else if (rampTimer.get() < RobotMap.TANK_RAMP_TIME) {
+            magnitude = magnitude * (rampTimer.get() / RobotMap.TANK_RAMP_TIME);
+        }
+        
+        
+        
         double dirInRad = direction + (Math.PI/4);
         
         double sinD = Math.sin(dirInRad);
