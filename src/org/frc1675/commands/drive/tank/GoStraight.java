24ad715c75d4b3ce1e885d8da0422fe8518ec8bb
Josh Pordon <josh.pordon@gmail.com>
2013-02-02 12:26:01 -0600

    changing gostraight to go to a specific distance

--- src/org/frc1675/commands/drive/tank/GoStraight.java
+++ src/org/frc1675/commands/drive/tank/GoStraight.java
@@ -4,6 +4,7 @@
  */
 package org.frc1675.commands.drive.tank;
 
+import org.frc1675.RobotMap;
 import org.frc1675.commands.CommandBase;
 
 /**
@@ -11,19 +12,19 @@ import org.frc1675.commands.CommandBase;
  * @author team1675
  */
 public class GoStraight extends CommandBase {
-    double setpoint = 0;
+    double setpoint;
     
     
-    public GoStraight() {
+    public GoStraight(double distanceInFeet) {
         requires (rightDrivePID);
         requires (leftDrivePID);
-
+        setpoint = calculateSetpoint(distanceInFeet);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        rightDrivePID.setSetpoint(3600);
-        leftDrivePID.setSetpoint(3600);
+        rightDrivePID.setSetpoint((int)setpoint);
+        leftDrivePID.setSetpoint((int)setpoint);
         
         rightDrivePID.enable();
         leftDrivePID.enable();
@@ -58,5 +59,9 @@ public class GoStraight extends CommandBase {
     // subsystems is scheduled to run
     protected void interrupted() {
         end();
+    }
+
+    private double calculateSetpoint(double distanceInFeet) {
+        return distanceInFeet * (RobotMap.DRIVE_ENCODER_TICKS_PER_REV / (RobotMap.DRIVE_WHEEL_DIAMETER * Math.PI));
     }
 }
