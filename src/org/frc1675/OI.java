e3be1d0a6149fedf5693482ca2c1385bbfa7bdd1
Josh Pordon <josh.pordon@gmail.com>
2013-03-24 17:08:49 -0500

    Post-WIMI cleanup: removed extraneous comments from OI

--- src/org/frc1675/OI.java
+++ src/org/frc1675/OI.java
@@ -39,46 +39,8 @@ public class OI {
     private Button operatorLeftJoystickButton = new JoystickButton(operatorController, XBoxControllerMap.RIGHT_JOYSTICK_BUTTON);
     private Button operatorRightJoystickButton = new JoystickButton(operatorController, XBoxControllerMap.LEFT_JOYSTICK_BUTTON);
     
-    //testing pulling from master
-
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-    // Another type of button you can create is a DigitalIOButton, which is
-    // a button or switch hooked up to the cypress module. These are useful if
-    // you want to build a customized operator interface.
-    // Button button = new DigitalIOButton(1);
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.Attempted to add PID controllers.  
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three wahave a button, it's trivial to bind it to a button in one of
-    //// TRIGGERINGys:
-    // Once you have a button, it's trivial to bind it to a button in one of
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
-
-    //HOLY COMMENT
     public OI() {
 
-
-//        driverXButton.whenPressed(new GoToAngle(-90));
-//        driverBButton.whenPressed(new GoToAngle(90));
-//        driverYButton.whenPressed(new ClimberExtend());
-//        driverAButton.whenPressed(new ClimberRetract());
         operatorXButton.whenPressed(new FootDown());
         operatorBButton.whenPressed(new FootUp());
         operatorYButton.whenPressed(new ClimberExtend());
@@ -90,15 +52,6 @@ public class OI {
     public double getMecanumMagnitude() {
         double x = driverController.getRawAxis(XBoxControllerMap.LEFT_X_AXIS);
         double y = driverController.getRawAxis(XBoxControllerMap.LEFT_Y_AXIS);
-        System.out.println("Controller x: " + x + " , y: " + y);
-//        double magnitude = Math.sqrt(MathUtils.pow(x, 2.0) + MathUtils.pow(y, 2.0));
-//
-//        if (magnitude < RobotMap.DEADZONE_RADIUS) {
-//            magnitude = 0;
-//        }
-//
-//
-//        return magnitude;
         double returnMagnitude = 0.0;
         double squareMagnitude = Math.sqrt(MathUtils.pow(x, 2.0) + MathUtils.pow(y, 2.0));
         if(x != 0.0 && y != 0.0){
@@ -148,7 +101,6 @@ public class OI {
             direction = MathUtils.atan2(y, x);
         }
         direction -= Math.PI / 2;
-//        System.out.println(direction);
 
         return direction;
     }
