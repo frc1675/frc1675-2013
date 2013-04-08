bd2c9cdecda5a5a7d5421fc5cf3424d08f0f82a1
Josh Pordon <josh.pordon@gmail.com>
2013-04-08 16:32:41 -0500

    ILCH: Made auton more customizable

--- src/org/frc1675/commands/BackCornerAuton.java
+++ src/org/frc1675/commands/BackCornerAuton.java
@@ -14,14 +14,20 @@ import org.frc1675.commands.shooter.GoToShootingSpeed;
  * @author josh
  */
 public class BackCornerAuton extends CommandGroup {
+    
+    private static final double INITIAL_WIND_UP = 6.0;
+    private static final double AFTER_FIRST_SHOT = 3.0;
+    private static final double AFTER_SECOND_SHOT = 3.0;
+    private static final double DRIVE_DURATION = 1.0;
+
     public BackCornerAuton() {
         addSequential(new GoToShootingSpeed());
-        addSequential(new Wait(6.0));
+        addSequential(new Wait(INITIAL_WIND_UP));
         addSequential(new Index());
-        addSequential(new Wait(3.0));
+        addSequential(new Wait(AFTER_FIRST_SHOT));
         addSequential(new Index());
-        addSequential(new Wait(3.0));
+        addSequential(new Wait(AFTER_SECOND_SHOT));
         addSequential(new Index());
-        addSequential(new MecanumDrivePolarForTime(0.5, 0.0, 0.0, 1.0));
+        addSequential(new MecanumDrivePolarForTime(0.5, 0.0, 0.0, DRIVE_DURATION));
     }
 }
